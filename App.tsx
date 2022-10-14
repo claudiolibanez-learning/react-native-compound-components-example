ff36105f836642ee4f802bcb928a65c809b503f8
Luis Claudio Soares Silva <luis.silva@actdigital.com>
2022-10-14 03:03:29 -0300

    feat: add textinput

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { TextInput } from "./src/components/TextInput";
+
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World</Text>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 20
+    }}>
+      <TextInput.Container
+        label="E-mail"
+      >
+        <TextInput.Input
+          placeholder="Digite seu e-mail"
+        />
+      </TextInput.Container>
     </View>
   );
 }
